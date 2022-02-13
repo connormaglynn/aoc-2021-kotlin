55eac1cc3b56fc2ebaf58b765cea1b65ad9d0908
Connor Glynn <connor.glynn@digital.justice.gov.uk>
2022-02-13 23:32:43 +0000

    calculate measurements larger than previous grouped by 3

--- app/src/main/kotlin/aoc/sonar/sweep/kotlin/SonarSystem.kt
+++ app/src/main/kotlin/aoc/sonar/sweep/kotlin/SonarSystem.kt
@@ -1,6 +1,6 @@
 package aoc.sonar.sweep.kotlin
 
-class SonarSystem() {
+class SonarSystem {
     fun measurementsLargerThanPrevious(input: List<Int>): Int {
         var counter = 0
         var previousValue: Int? = null
@@ -12,5 +12,21 @@ class SonarSystem() {
         }
 
         return counter
+    }
+
+    fun measurementsLargerThanPreviousGroupedBy3(input: List<Int>): Int {
+        val inputGrouped: MutableList<Int> = mutableListOf()
+        input.forEachIndexed { index, i ->
+            try {
+                val secondValue = input[index + 1]
+                val thirdValue = input[index + 2]
+                val sumOfAll = i + secondValue + thirdValue
+                inputGrouped += sumOfAll
+            } catch (e: Exception) {
+                return@forEachIndexed
+            }
+        }
+
+        return measurementsLargerThanPrevious(inputGrouped)
     }
 }
--- app/src/test/kotlin/aoc/sonar/sweep/kotlin/SonarSystemTest.kt
+++ app/src/test/kotlin/aoc/sonar/sweep/kotlin/SonarSystemTest.kt
@@ -9,30 +9,46 @@ import kotlin.test.assertEquals
 class SonarSystemTest {
 
     private lateinit var sonarSystem: SonarSystem
+    private val noInput: List<Int> = listOf()
+    private val smallInput = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+    private val day1Input: MutableList<Int> = mutableListOf()
+    private val day1InputFileLocation = "src/test/resources/day1Input.txt"
 
     @Before
     fun before() {
         sonarSystem = SonarSystem()
+        File(day1InputFileLocation).readLines().forEach {
+            day1Input += parseInt(it)
+        }
     }
 
     @Test
     fun `test measurementsLargerThanPrevious no input`() {
-        val input: List<Int> = listOf()
-        assertEquals(0, sonarSystem.measurementsLargerThanPrevious(input))
+        assertEquals(0, sonarSystem.measurementsLargerThanPrevious(noInput))
     }
 
     @Test
     fun `test measurementsLargerThanPrevious small input`() {
-        val input = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
-        assertEquals(7, sonarSystem.measurementsLargerThanPrevious(input))
+        assertEquals(7, sonarSystem.measurementsLargerThanPrevious(smallInput))
     }
 
     @Test
-    fun `test measurementsLargerThanPrevious small large input`() {
-        val input: MutableList<Int> = mutableListOf()
-        File("src/test/resources/day1Input.txt").readLines().forEach {
-            input += parseInt(it)
-        }
-        assertEquals(1665, sonarSystem.measurementsLargerThanPrevious(input))
+    fun `test measurementsLargerThanPrevious large input`() {
+        assertEquals(1665, sonarSystem.measurementsLargerThanPrevious(day1Input))
+    }
+
+    @Test
+    fun `test measurementsLargerThanPreviousGroupedBy3 no input`() {
+        assertEquals(0, sonarSystem.measurementsLargerThanPreviousGroupedBy3(noInput))
+    }
+
+    @Test
+    fun `test measurementsLargerThanPreviousGroupedBy3 small input`() {
+        assertEquals(5, sonarSystem.measurementsLargerThanPreviousGroupedBy3(smallInput))
+    }
+
+    @Test
+    fun `test measurementsLargerThanPreviousGroupedBy3 large input`() {
+        assertEquals(1702, sonarSystem.measurementsLargerThanPreviousGroupedBy3(day1Input))
     }
 }
